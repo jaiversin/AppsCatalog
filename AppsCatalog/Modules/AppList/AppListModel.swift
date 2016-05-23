a10a2743a2fe28070c4c81cb37ebf96e7decbe7d
jaiversin <jaiversin@gmail.com>
2016-05-23 00:20:28 -0500

    Initial config for App List

--- AppsCatalog/Modules/AppList/AppListModel.swift
+++ AppsCatalog/Modules/AppList/AppListModel.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+struct AppListModel {
+    let name: String
+    let iconPath: String
+    let summary: String
+    let price: String
+    let categoryId: String
+    let categoryName: String
+    
+    init(name: String, iconPath: String, summary: String, price: String, categoryId: String, categoryName: String){
+        
+        self.name = name
+        self.iconPath = iconPath
+        self.summary = summary
+        self.price = price
+        self.categoryId = categoryId
+        self.categoryName = categoryName
+        
+    }
+    
+}
---
+++ AppsCatalog/Modules/AppList/AppListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AppListViewController.swift
+//  AppsCatalog
+//
+//  Created by Jhon López on 5/23/16.
+//  Copyright © 2016 jaiversin. All rights reserved.
+//
+
+import UIKit
+
+class AppListViewController: UIViewController {
+
+}
