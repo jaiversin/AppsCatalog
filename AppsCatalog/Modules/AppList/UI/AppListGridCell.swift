10b25a85ec055595d4c78f89f7d1b3671601fd9e
jaiversin <jaiversin@gmail.com>
2016-05-24 00:22:39 -0500

    Added grid cell and collection view config

---
+++ AppsCatalog/Modules/AppList/UI/AppListGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  AppListGridCell.swift
+//  AppsCatalog
+//
+//  Created by Jhon López on 5/24/16.
+//  Copyright © 2016 jaiversin. All rights reserved.
+//
+
+import Cocoa
+
+class AppListGridCell: UICollectionViewCell {
+
+}
